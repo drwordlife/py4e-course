45e6f578a737d22cfe3d69965c3222cf5e9324d5
Andrew Sandoval <alsandoval01@gmail.com>
2020-10-06 15:57:43 +0800

    Untrack hidden.py

--- hidden.py
+++
@@ -1,5 +0,0 @@
-def oauth():
-    return {"consumer_key": "XV8c1FECRLSSqaTyGKAMoabdO",
-            "consumer_secret": "Tip58KPLD8pMLGYNHqk8AjGkYkV5MJrSlMmEJojFKzUak5heFJ",
-            "token_key": "142726380-1goXeSKLB2V2sFZd2ZNIc19XDoiltxIzkpNfOxvX",
-            "token_secret": "3poAGJU2PHuHRiFz65MAEaWzIPv4T0zQCRIJRi3L8eODs"}
